2f6df9bb5644c6f1a11d09e2af674880e6194396
righi.simone <righi.simone@tave05.OSDBSESTO.local>
2014-03-12 11:10:11 +0100

    Aggiunta terminazione

--- src/echoclient/EchoClient.java
+++ src/echoclient/EchoClient.java
@@ -5,9 +5,10 @@ import java.net.*;
 
 public class EchoClient {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException, NullPointerException {
 
         Socket s = new Socket("127.0.0.1", 6666);
+        String toServer;
         BufferedReader sock_in = new BufferedReader(new InputStreamReader(s.getInputStream()));
         PrintWriter sock_out = new PrintWriter(new OutputStreamWriter(s.getOutputStream()), true);
         BufferedReader std_in = new BufferedReader(new InputStreamReader(System.in));
@@ -16,7 +17,12 @@ public class EchoClient {
         while (true) {
             sock_out.println(std_in.readLine());
             //std_out.println(sock_in.readLine());
-            System.out.println(sock_in.readLine());
+            toServer = sock_in.readLine();
+            System.out.println(toServer);
+            if(toServer.equalsIgnoreCase("Terminato")){
+                s.close();
+                System.exit(0);
+            }
         }
     }
 }
